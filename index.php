dc120daf3471172ed3dd9e3cab450f069d750ebc
AndrewKahora <cahndrew@gmail.com>
2016-09-22 19:10:03 +0300

    creating php to responsive email

---
+++ index.php
@@ -0,0 +1,16 @@
+<?php
+    $name = $_POST['name'];
+    $email = $_POST['email'];
+    $message = $_POST['message'];
+    $from = 'From: Druhs portfolio;
+    $to = 'cahndrew@gmail.com';
+    $subject = 'Hello';
+
+    $body = "From: $name\n E-Mail: $email\n Message:\n $message";
+?>
+
+<?php
+if ($_POST['submit']) {
+    /* Anything that goes in here is only performed if the form is submitted */
+}
+?>
