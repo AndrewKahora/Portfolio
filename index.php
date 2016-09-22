33088c7a57fbe7e925e178d03a96fa8dd4ae5376
AndrewKahora <cahndrew@gmail.com>
2016-09-22 19:23:39 +0300

    contact form php

--- index.php
+++ index.php
@@ -1,20 +1,3 @@
-<html>
-<?php
-    $name = $_POST['name'];
-    $email = $_POST['email'];
-    $message = $_POST['message'];
-    $from = 'From: Druhs portfolio;
-    $to = 'cahndrew@gmail.com';
-    $subject = 'Hello';
-
-    $body = "From: $name\n E-Mail: $email\n Message:\n $message";
-?>
-
-<?php
-if ($_POST['submit']) {
-    /* Anything that goes in here is only performed if the form is submitted */
-}
-?>
 
 <?php
     $name = $_POST['name'];
@@ -32,5 +15,5 @@ if ($_POST['submit']) {
     /* Anything that goes in here is only performed if the form is submitted */
 }
 ?>
-</html>
+
 
