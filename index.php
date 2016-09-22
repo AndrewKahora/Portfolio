1915b8f1e496cff2eeba34ce6c144d0ce595dc0f
AndrewKahora <cahndrew@gmail.com>
2016-09-22 19:21:29 +0300

    php

--- index.php
+++ index.php
@@ -1,3 +1,4 @@
+<html>
 <?php
     $name = $_POST['name'];
     $email = $_POST['email'];
@@ -14,3 +15,22 @@ if ($_POST['submit']) {
     /* Anything that goes in here is only performed if the form is submitted */
 }
 ?>
+
+<?php
+    $name = $_POST['name'];
+    $email = $_POST['email'];
+    $message = $_POST['message'];
+    $from = 'From: Druhs portfolio;
+    $to = 'cahndrew@gmail.com';
+    $subject = 'Hello';
+
+    $body = "From: $name\n E-Mail: $email\n Message:\n $message";
+?>
+
+<?php
+if ($_POST['submit']) {
+    /* Anything that goes in here is only performed if the form is submitted */
+}
+?>
+</html>
+
